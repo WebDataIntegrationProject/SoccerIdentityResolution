e43a4f529d0a3a3412d106f968f26473675f8785
sturmti <timo.sturm@googlemail.com>
2017-11-06 22:56:41 +0900

    added new date, new name & height comparators

---
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/SoccerIdentityResolution/comparators/PlayerBirthDateComparatorDateComparison.java
@@ -0,0 +1,33 @@
+package de.uni_mannheim.informatik.dws.wdi.SoccerIdentityResolution.comparators;
+
+import de.uni_mannheim.informatik.dws.wdi.SoccerIdentityResolution.model.Player;
+import de.uni_mannheim.informatik.dws.winter.matching.rules.Comparator;
+import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
+import de.uni_mannheim.informatik.dws.winter.model.Matchable;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.uni_mannheim.informatik.dws.winter.similarity.string.LevenshteinSimilarity;
+import java.lang.*;
+
+public class PlayerBirthDateComparatorDateComparison implements Comparator<Player, Attribute> {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public double compare(Player record1, Player record2, Correspondence<Attribute, Matchable> schemaCorrespondence) {
+
+    	double sim = 0.0;
+    	
+        if(record1.getBirthDate() == null || record2.getBirthDate() == null){
+            return 0.0;
+        }
+
+        if(record1.getBirthDate().equals(record2.getBirthDate())){
+        	sim = 1.0;
+        }
+
+        return sim;
+
+    }
+
+
+}
---
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/SoccerIdentityResolution/comparators/PlayerHeightComparator.java
@@ -0,0 +1,35 @@
+package de.uni_mannheim.informatik.dws.wdi.SoccerIdentityResolution.comparators;
+
+import de.uni_mannheim.informatik.dws.wdi.SoccerIdentityResolution.model.Player;
+import de.uni_mannheim.informatik.dws.winter.matching.rules.Comparator;
+import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
+import de.uni_mannheim.informatik.dws.winter.model.Matchable;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.uni_mannheim.informatik.dws.winter.similarity.string.LevenshteinSimilarity;
+import java.lang.*;
+
+public class PlayerHeightComparator implements Comparator<Player, Attribute> {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public double compare(Player record1, Player record2, Correspondence<Attribute, Matchable> schemaCorrespondence) {
+
+    	int sim = 0;
+    	
+        if(record1.getHeight() == null || record2.getHeight() == null){
+            return 0.0;
+        }
+        
+        int dmax = 2;
+        
+        if(Math.abs(record1.getHeight() - record2.getHeight()) <= dmax){
+        	sim = 1 - (Math.abs(record1.getHeight() - record2.getHeight())/dmax);
+        }
+       
+        return sim;
+
+    }
+
+
+}
