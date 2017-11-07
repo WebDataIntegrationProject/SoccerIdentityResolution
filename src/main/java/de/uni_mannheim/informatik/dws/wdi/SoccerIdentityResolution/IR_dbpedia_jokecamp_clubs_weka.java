3bd72836c92cab0d2ad0dc80f437314a83ebacd9
dhentschel06 <hentschel.david@gmail.com>
2017-11-07 10:00:06 +0100

    small tryout with weka + club blocker by league + gs dbpedia jokecamp clubs + option for lowercase with levensthein

---
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/SoccerIdentityResolution/IR_dbpedia_jokecamp_clubs_weka.java
@@ -0,0 +1,86 @@
+package de.uni_mannheim.informatik.dws.wdi.SoccerIdentityResolution;
+
+import java.io.File;
+
+import de.uni_mannheim.informatik.dws.wdi.SoccerIdentityResolution.model.Club;
+import de.uni_mannheim.informatik.dws.wdi.SoccerIdentityResolution.model.ClubXMLReader;
+import de.uni_mannheim.informatik.dws.wdi.SoccerIdentityResolution.blockers.ClubBlockerByLeague;
+import de.uni_mannheim.informatik.dws.wdi.SoccerIdentityResolution.comparators.ClubNameComparatorLevenshtein;
+import de.uni_mannheim.informatik.dws.winter.matching.MatchingEngine;
+import de.uni_mannheim.informatik.dws.winter.matching.MatchingEvaluator;
+import de.uni_mannheim.informatik.dws.winter.matching.algorithms.RuleLearner;
+import de.uni_mannheim.informatik.dws.winter.matching.blockers.NoBlocker;
+import de.uni_mannheim.informatik.dws.winter.matching.blockers.StandardRecordBlocker;
+import de.uni_mannheim.informatik.dws.winter.matching.rules.WekaMatchingRule;
+import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
+import de.uni_mannheim.informatik.dws.winter.model.HashedDataSet;
+import de.uni_mannheim.informatik.dws.winter.model.MatchingGoldStandard;
+import de.uni_mannheim.informatik.dws.winter.model.Performance;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.uni_mannheim.informatik.dws.winter.model.io.CSVCorrespondenceFormatter;
+import de.uni_mannheim.informatik.dws.winter.processing.Processable;
+
+public class IR_dbpedia_jokecamp_clubs_weka
+{
+    public static void main( String[] args ) throws Exception
+    {
+        // loading data
+        HashedDataSet<Club, Attribute> dataDbpedia = new HashedDataSet<>();
+        new ClubXMLReader().loadFromXML(new File("data/input/dbpedia.xml"), "/clubs/club", dataDbpedia);
+        HashedDataSet<Club, Attribute> dataJokecamp = new HashedDataSet<>();
+        new ClubXMLReader().loadFromXML(new File("data/input/jokecamp-others.xml"), "/clubs/club", dataJokecamp);
+
+        System.out.println("Sample from dbpedia: " + dataDbpedia.getRandomRecord());
+        System.out.println("Sample from jokecamp: " + dataJokecamp.getRandomRecord());
+
+        // create a matching rule
+        String options[] = new String[] { "" };
+        String modelType = "SimpleLogistic";
+        WekaMatchingRule<Club, Attribute> matchingRule = new WekaMatchingRule<>(0.6, modelType, options);
+        
+        // add comparators
+        matchingRule.addComparator(new ClubNameComparatorLevenshtein(false));
+        matchingRule.addComparator(new ClubNameComparatorLevenshtein(true));
+        
+        // load the training set
+        MatchingGoldStandard gsTraining = new MatchingGoldStandard();
+        gsTraining.loadFromCSVFile(new File("data/goldstandard/gs_dbpedia_jokecamp_clubs.csv"));
+        
+        // train the matching rule's model
+        RuleLearner<Club, Attribute> learner = new RuleLearner<>();
+        learner.learnMatchingRule(dataDbpedia, dataJokecamp, null, matchingRule, gsTraining);
+
+        // create a blocker (blocking strategy)
+		StandardRecordBlocker<Club, Attribute> blocker = new StandardRecordBlocker<Club, Attribute>(new ClubBlockerByLeague());
+
+        // Initialize Matching Engine
+        MatchingEngine<Club, Attribute> engine = new MatchingEngine<>();
+
+        // Execute the matching
+        Processable<Correspondence<Club, Attribute>> correspondences = engine.runIdentityResolution(
+                dataDbpedia, dataJokecamp, null, matchingRule,
+                blocker);
+
+        // write the correspondences to the output file
+        new CSVCorrespondenceFormatter().writeCSV(new File("data/output/dbpedia_jokecamp_clubs_correspondences.csv"), correspondences);
+
+        // load the gold standard (test set)
+        MatchingGoldStandard gsTest = new MatchingGoldStandard();
+        gsTest.loadFromCSVFile(new File(
+                "data/goldstandard/gs_dbpedia_jokecamp_clubs.csv"));
+
+        // evaluate your result
+        MatchingEvaluator<Club, Attribute> evaluator = new MatchingEvaluator<Club, Attribute>(true);
+        Performance perfTest = evaluator.evaluateMatching(correspondences.get(),
+                gsTest);
+        new ErrorAnalysisClubs().printFalsePositives(correspondences, gsTest);
+        new ErrorAnalysisClubs().printFalseNegatives(dataDbpedia, dataJokecamp, correspondences, gsTest);
+        // print the evaluation result
+        System.out.println("Dbpedia <-> Kaggle");
+        System.out
+                .println(String.format(
+                        "Precision: %.4f\nRecall: %.4f\nF1: %.4f",
+                        perfTest.getPrecision(), perfTest.getRecall(),
+                        perfTest.getF1()));
+    }
+}
--- src/main/java/de/uni_mannheim/informatik/dws/wdi/SoccerIdentityResolution/comparators/ClubNameComparatorLevenshtein.java
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/SoccerIdentityResolution/comparators/ClubNameComparatorLevenshtein.java
@@ -12,12 +12,23 @@ public class ClubNameComparatorLevenshtein implements Comparator<Club, Attribute
 
 	private static final long serialVersionUID = 1L;
 	private LevenshteinSimilarity sim = new LevenshteinSimilarity();
+	private boolean lowerCase = false;
 
+	public ClubNameComparatorLevenshtein() {
+		super();
+	}
+	
+	public ClubNameComparatorLevenshtein(boolean lowerCase) {
+		super();
+		this.lowerCase = lowerCase;
+	}
+	
 	@Override
 	public double compare(
 			Club record1,
 			Club record2,
 			Correspondence<Attribute, Matchable> schemaCorrespondences) {
+		if (this.lowerCase) return sim.calculate(record1.getName().toLowerCase(), record2.getName().toLowerCase());
 		return sim.calculate(record1.getName(), record2.getName());
 	}
 
