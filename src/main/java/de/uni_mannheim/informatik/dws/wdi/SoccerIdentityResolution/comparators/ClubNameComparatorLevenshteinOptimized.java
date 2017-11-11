414c0d0fbceb5acc222bc180cbd0173788439b5c
janothan <janothan@users.noreply.github.com>
2017-11-11 22:25:46 +0100

    Gold Standard Corrections, IR clubs, comparators extended.

--- src/main/java/de/uni_mannheim/informatik/dws/wdi/SoccerIdentityResolution/comparators/ClubNameComparatorLevenshteinOptimized.java
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/SoccerIdentityResolution/comparators/ClubNameComparatorLevenshteinOptimized.java
@@ -1,5 +1,6 @@
 package de.uni_mannheim.informatik.dws.wdi.SoccerIdentityResolution.comparators;
 
+import de.uni_mannheim.informatik.dws.wdi.SoccerIdentityResolution.model.Club;
 import de.uni_mannheim.informatik.dws.wdi.SoccerIdentityResolution.model.Player;
 import de.uni_mannheim.informatik.dws.winter.matching.rules.Comparator;
 import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
@@ -7,33 +8,33 @@ import de.uni_mannheim.informatik.dws.winter.model.Matchable;
 import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
 import de.uni_mannheim.informatik.dws.winter.similarity.string.LevenshteinSimilarity;
 
-public class PlayerClubNameLevenshtein implements Comparator<Player, Attribute> {
+public class ClubNameComparatorLevenshteinOptimized implements Comparator<Club, Attribute> {
 
     private static final long serialVersionUID = 1L;
     LevenshteinSimilarity sim = new LevenshteinSimilarity();
     boolean useStringSimplifier = false;
 
-    public PlayerClubNameLevenshtein(boolean lowerCase) {
+    public ClubNameComparatorLevenshteinOptimized(boolean lowerCase) {
         super();
         useStringSimplifier = true;
     }
 
-    public PlayerClubNameLevenshtein() { super(); }
+    public ClubNameComparatorLevenshteinOptimized() { super(); }
 
 
     @Override
-    public double compare(Player record1, Player record2, Correspondence<Attribute, Matchable> schemaCorrespondence) {
+    public double compare(Club record1, Club record2, Correspondence<Attribute, Matchable> schemaCorrespondence) {
 
-        if(record1.getClubName() == null || record2.getClubName() == null){
+        if(record1.getName() == null || record2.getName() == null){
             return 0.0;
         }
 
-        String string1 = record1.getClubName();
-        String string2 = record2.getClubName();
+        String string1 = record1.getName();
+        String string2 = record2.getName();
 
         if(useStringSimplifier){
-            string1 = StringSimplifier.simplifyString(string1);
-            string2 = StringSimplifier.simplifyString(string2);
+            string1 = StringSimplifier.simplifyStringClubOptimized(string1);
+            string2 = StringSimplifier.simplifyStringClubOptimized(string2);
         }
 
         return sim.calculate(string1, string2);
