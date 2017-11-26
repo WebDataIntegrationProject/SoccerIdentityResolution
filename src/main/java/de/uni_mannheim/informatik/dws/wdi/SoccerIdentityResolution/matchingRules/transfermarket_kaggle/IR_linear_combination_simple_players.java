f3732a8d6f9513a2bbe1f0457b410d62894ec23d
sturmti <timo.sturm@googlemail.com>
2017-11-26 09:30:05 +0100

    updated linear comb kaggle & TM

--- src/main/java/de/uni_mannheim/informatik/dws/wdi/SoccerIdentityResolution/matchingRules/transfermarket_kaggle/IR_linear_combination_simple_players.java
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/SoccerIdentityResolution/matchingRules/transfermarket_kaggle/IR_linear_combination_simple_players.java
@@ -73,7 +73,7 @@ public class IR_linear_combination_simple_players
 
         // create a matching rule
         LinearCombinationMatchingRule<Player, Attribute> matchingRule = new LinearCombinationMatchingRule<>(
-                0.8);
+                0.9);
 
 
         // add comparators
@@ -86,9 +86,8 @@ public class IR_linear_combination_simple_players
         //matchingRule.addComparator(new PlayerNameComparatorMongeElkan(true, "jaroWinkler"), 1); // 0.9750  on preprocessed data
         //matchingRule.addComparator(new PlayerNameComparatorMongeElkan(true, "cosine"), 0.8);	// 0.8250  on preprocessed data
         //matchingRule.addComparator(new PlayerNameComparatorDoubleMetaphone(true), 0.8);		// 0.9333 (0.8*DoubleMetaphone + 0.2*Date > 0.8)  on preprocessed data
-        matchingRule.addComparator(new PlayerNameComparatorMongeElkan(true, "doubleMetaphone"), 0.8);	// F1:0.9873 [P:0.975, R:1.0] (0.8*MongeElkan(DoubleMetaphone) + 0.2*Date > 0.8)  on preprocessed data
-        
- 
+        matchingRule.addComparator(new PlayerNameComparatorMongeElkan(true, "doubleMetaphone"), 0.8);	// F1:0.9784 [P:0.9714, R:0.9855] (0.8*MongeElkan(DoubleMetaphone) + 0.2*Date > 0.9)  on preprocessed data
+    
         matchingRule.addComparator(new PlayerBirthDateComparatorExactDateComparison(), 0.2);
 
         // create a blocker (blocking strategy)
