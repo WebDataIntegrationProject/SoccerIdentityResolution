abf595f9bb86844fa48dd61c0b98370af979d115
I852496 <timo.sturm02@sap.com>
2017-12-01 15:48:33 +0100

    updated explanation of altnerativeClubChecker

--- src/main/java/de/uni_mannheim/informatik/dws/wdi/SoccerIdentityResolution/comparators/AlternativeClubChecker.java
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/SoccerIdentityResolution/comparators/AlternativeClubChecker.java
@@ -13,15 +13,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * A simplifier which allows to better compare input strings.
- * The following operations are implemented:
- * - lowercasing
- * - removing leading and trailing white spaces
- * - removing ".", ";", ":" and ","
- * - replacing accents with the original letter e.g. á → a
- * - handling of German umlauts and "ß"
- *
- * Please adapt the unit test if you change something!
+ * Used to check whether a club is an alternative version of a main club (e.g. "Bayern Munich II" or "FC Köln U18")
  */
 public class AlternativeClubChecker {
 
